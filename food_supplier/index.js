b91935e73b3f25011436d860786c86a6c1223788
Aaron <aaron.ciaghi@gmail.com>
2011-06-04 17:26:12 +0200

    clean up

--- food_supplier/index.js
+++ food_supplier/index.js
@@ -1,71 +1,77 @@
-Ext.regModel('File', {
-    idProperty: 'id',
-    fields: [
-        {name: 'id',       type: 'string'},
-        {name: 'fileName', type: 'string'}
-    ]
-});
-
-
 Ext.setup({
     icon: 'icon.png',
     tabletStartupScreen: 'tablet_startup.png',
     phoneStartupScreen: 'phone_startup.png',
     glossOnIcon: false,
     onReady: function(){
+        var buttons = [
+        {
+            text: 'Normal'
+        },
+        {
+            ui  : 'round',
+            text: 'Round'
+        },
+        {
+            ui  : 'small',
+            text: 'Small'
+        }
+        ];
 
-        var store = new Ext.data.TreeStore({
-            model: 'File',
-            proxy: {
-                type: 'ajax',
-                url: 'getSourceFiles.php',
-                reader: {
-                    type: 'tree',
-                    root: 'children'
-                }
-            }
-        });
-
-
-        var nestedList = new Ext.NestedList({
-            fullscreen: true,
-            title: 'Food Supplier',
-            displayField: 'fileName',
-            // add a / for folder nodes in title/back button
-            getTitleTextTpl: function() {
-                return '{' + this.displayField + '}<tpl if="leaf !== true">/</tpl>';
+        var panel = new Ext.Panel({
+            layout: {
+                type : 'vbox',
+                pack : 'center',
+                align: 'stretch'
             },
-            // add a / for folder nodes in the list
-            getItemTextTpl: function() {
-                return '{' + this.displayField + '}<tpl if="leaf !== true">/</tpl>';
-            },
-            // provide a codebox for each source file
-            getDetailCard: function(record, parentRecord) {
-                return new Ext.ux.CodeBox({
-                    value: 'Loading...',
-                    scroll: {
-                        direction: 'both',
-                        eventTarget: 'parent'
-                    }
-                });
-            },
-            store: store
-        });
-
-
-        nestedList.on('leafitemtap', function(subList, subIdx, el, e, detailCard) {
-            var ds = subList.getStore(),
-                r  = ds.getAt(subIdx);
-
-            Ext.Ajax.request({
-                url: '../../src/' + r.get('id'),
-                success: function(response) {
-                    detailCard.setValue(response.responseText);
+            defaults: {
+                layout: {
+                    type: 'hbox'
                 },
-                failure: function() {
-                    detailCard.setValue("Loading failed.");
+                flex: 1,
+                defaults: {
+                    xtype: 'button',
+                    cls  : 'demobtn',
+                    flex : 1
                 }
-            });
+            },
+            items: [
+            {
+                items: buttons // buttons array defined above
+            },
+            {
+                items: [
+                new Ext.Button({
+                    ui  : 'decline',
+                    text: 'Drastic'
+                }),
+                {
+                    ui  : 'decline-round',
+                    text: 'Round'
+                },
+                {
+                    ui  : 'decline-small',
+                    text: 'Small'
+                }
+                ]
+            },
+            {
+                items: [
+                {
+                    ui  : 'confirm',
+                    text: 'Confirm'
+                },
+                {
+                    ui  : 'confirm-round',
+                    text: 'Round'
+                },
+                {
+                    ui  : 'confirm-small',
+                    text: 'Small'
+                }
+                ]
+            }
+            ]
         });
     }
 });
