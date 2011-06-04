23a0e7f9e6ee4cfa40ea90b2a2962192d476881f
Aaron <aaron.ciaghi@gmail.com>
2011-06-04 18:12:13 +0200

    prepared template for offers javascript

---
+++ food_supplier/js/offers.js
@@ -0,0 +1,9 @@
+Ext.setup({
+    icon: 'icon.png',
+    tabletStartupScreen: 'tablet_startup.png',
+    phoneStartupScreen: 'phone_startup.png',
+    glossOnIcon: false,
+    onReady: function(){
+       
+    }
+});
