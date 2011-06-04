b715d85c3bb51aa9330460bc74ec695d52819091
Andrea <andreamattioli@hulk.local>
2011-06-04 17:01:48 +0200

    main mage

---
+++ index.php
@@ -0,0 +1,12 @@
+<hmtl>
+    <head>Moving Foods</head>
+
+    <body>
+        <p><?php ?></p>
+        <ul>
+            <li><a href="food_supplier/index.html">Supplier</a></li>
+           <li>Collector</li>
+           <li>Distributor</li>
+        </ul>
+    </body>
+</html>
