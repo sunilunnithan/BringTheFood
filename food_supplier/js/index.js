3868026028e7e38bd95a4f0a69f363aedabccde0
Aaron <aaron.ciaghi@gmail.com>
2011-06-04 18:09:51 +0200

    finally something works!!

--- food_supplier/js/index.js
+++ food_supplier/js/index.js
@@ -1,71 +1,46 @@
-Ext.regModel('File', {
-    idProperty: 'id',
-    fields: [
-        {name: 'id',       type: 'string'},
-        {name: 'fileName', type: 'string'}
-    ]
-});
-
-
 Ext.setup({
     icon: 'icon.png',
     tabletStartupScreen: 'tablet_startup.png',
     phoneStartupScreen: 'phone_startup.png',
     glossOnIcon: false,
     onReady: function(){
-
-        var store = new Ext.data.TreeStore({
-            model: 'File',
-            proxy: {
-                type: 'ajax',
-                url: 'getSourceFiles.php',
-                reader: {
-                    type: 'tree',
-                    root: 'children'
-                }
-            }
+        var offersBtn = new Ext.Button({
+            text: 'Offers',
+            ui: 'round',
+            handler: function(){location.href = 'offers.php'}
         });
 
-
-        var nestedList = new Ext.NestedList({
-            fullscreen: true,
-            title: 'src/',
-            displayField: 'fileName',
-            // add a / for folder nodes in title/back button
-            getTitleTextTpl: function() {
-                return '{' + this.displayField + '}<tpl if="leaf !== true">/</tpl>';
-            },
-            // add a / for folder nodes in the list
-            getItemTextTpl: function() {
-                return '{' + this.displayField + '}<tpl if="leaf !== true">/</tpl>';
-            },
-            // provide a codebox for each source file
-            getDetailCard: function(record, parentRecord) {
-                return new Ext.ux.CodeBox({
-                    value: 'Loading...',
-                    scroll: {
-                        direction: 'both',
-                        eventTarget: 'parent'
-                    }
-                });
-            },
-            store: store
+        var adsBtn = new Ext.Button({
+            text: 'Ads',
+            ui: 'round'
         });
 
-
-        nestedList.on('leafitemtap', function(subList, subIdx, el, e, detailCard) {
-            var ds = subList.getStore(),
-                r  = ds.getAt(subIdx);
-
-            Ext.Ajax.request({
-                url: '../../src/' + r.get('id'),
-                success: function(response) {
-                    detailCard.setValue(response.responseText);
-                },
-                failure: function() {
-                    detailCard.setValue("Loading failed.");
-                }
-            });
+        var accountBtn = new Ext.Button({
+            text: 'My Account',
+            ui: 'round'
         });
+
+        new Ext.Panel({
+            fullscreen: 'true',
+            layout: {
+                type : 'vbox',
+                pack : 'center',
+                align: 'stretch'
+            },
+            defaults:{
+                flex: 1
+            },
+            dockedItems: [
+            {
+                dock: 'top',
+                xtype: 'toolbar',
+                title: 'Food Supplier'
+            },
+            offersBtn,
+            adsBtn,
+            accountBtn
+            ]
+        });
+        
     }
 });
