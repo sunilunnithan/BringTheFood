cbe0926ed295ce89fab66f4b30a9ae5c2dd7af5a
birhanum <birhanum@walia.local>
2011-06-05 11:11:13 +0200

    JSON object fixed

--- food_supplier/inc/functions.php
+++ food_supplier/inc/functions.php
@@ -4,11 +4,11 @@
  * and open the template in the editor.
  */
 
-include ('../db/DB_Connector.php');
+include ('../../db/DB_Connector.php');
 connectToMySQL();
 
 function supplier_JSON($ID){
-    $supplier =mysql_query("SELECT * FROM supplier WHERE supplier_ID ='$ID'") or die ('Unable to retrieve supplier');
+    $supplier =mysql_query("SELECT * FROM supplier WHERE supplier_ID='$ID'") or die ('Unable to retrieve supplier'. mysql_error());
     if ($supplier){
             $supplier_ID=mysql_result($supplier, 0, 'supplier_ID');
             $name= mysql_result($supplier, 0, 'name');
@@ -17,12 +17,10 @@ function supplier_JSON($ID){
             $email=mysql_result($supplier, 0, 'email');
             $this_supplier =array ('supplier_name'=>$name,'supplier_address'=>$address,'supplier_phone'=>$phone,'supplier_email'=>$email);
             $supplier_JSON =json_encode($this_supplier);
-            echo $supplier_JSON;
-            return $supplier_JSON;
-         
-}
+            //echo $supplier_JSON;
+            return $supplier_JSON; 
+    }
 
-echo supplier_JSON(1);
 }
 
 ?>
