0610d8a311380e9c865fbda6e26f42f7846e9ede
birhanum <birhanum@walia.local>
2011-06-21 14:50:35 +0200

    small refactoring

--- config/class.uManagement.php
+++ config/class.uManagement.php
@@ -1,6 +1,5 @@
 <?php
-define("MAPS_HOST", "maps.google.com");
-define("KEY", 'ABQIAAAA0rgRviA_63qGVWEKdx8ZOxRYrjFVhF5kx3H2A1TMuRZMY43TWRR7RygmrBmV4H-NDeem5LnW9Lo_Cw');
+include ('../config/geocode.php');
 
 // 
 // ---------------------------------------------------------------------------
@@ -215,7 +214,7 @@ Returns false on Error
 
            //Preparing address to fetch geocode.
             $complete_address = $info["street"].", ".$info["city"].", ".$info["zip"].", ".$info["country"];
-            $geocode_info = $this->grapGeocodeInfo($complete_address);
+            $geocode_info = grapGeocodeInfo($complete_address);
             $lat = $geocode_info["lat"];
             $lng = $geocode_info["lng"];
             $sql_address = "INSERT INTO {$table_name} (address_type, address_type_id, $into_tbladdress,lat,lng)
@@ -326,7 +325,7 @@ On Failure return false
         $complete_address = $info["street"] . ", " . $info["city"] . ", " . $info["zip"] . ", " . $info["country"];
 
         //Now get the geocode.
-        $geocode_info = $this->grapGeocodeInfo($complete_address);
+        $geocode_info = grapGeocodeInfo($complete_address);
         $lat = $geocode_info["lat"];
         $lng = $geocode_info["lng"];
 
@@ -510,58 +509,7 @@ Returns false on error
            return false;
     }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////
-/*
-Get geocode information given an address and populate into a database.
--Takes one parameter
-	@full address = string (an address composed )
-					Example:
-					via Sommarive 14, 38123, Trento, Italy
-Returns the geocode location for that address in terms of lat and lng.
-*/
-/////////////////////////////////////////////////////////////////////////////////////////////
-    function grapGeocodeInfo($addr) {
 
-        // Initialize delay in geocode speed
-        //echo 'Address received '.$addr."<br />";
-        $geocodeInfo = null;
-        $delay = 0;
-        $base_url = "http://" . MAPS_HOST . "/maps/geo?output=xml" . "&key=" . KEY;
-
-        $geocode_pending = true;
-
-        while ($geocode_pending) {
-
-            $request_url = $base_url . "&q=" . urlencode($addr);
-            $xml = simplexml_load_file($request_url) or die("url not loading");
-
-            $status = $xml->Response->Status->code;
-            //echo 'status: ' . $status . "<br />";
-            if (strcmp($status, "200") == 0) {
-                // Successful geocode
-                $geocode_pending = false;
-                $coordinates = $xml->Response->Placemark->Point->coordinates;
-                $coordinatesSplit = split(",", $coordinates);
-                // Format: Longitude, Latitude, Altitude
-                $lat = $coordinatesSplit[1];
-                $lng = $coordinatesSplit[0];
-                $geocodeInfo = array("addr" => $addr, "lat" => $lat, "lng" => $lng, "status" => $status);
-
-                //echo 'lat : ' . $lat . " and lng: " . $lng . "<br />";
-            } else if (strcmp($status, "620") == 0) {
-                // sent geocodes too fast
-                $delay += 100000;
-            } else {
-                // failure to geocode
-                $geocode_pending = false;
-                $geocodeInfo = array("addr" => $addr, "lat" => NULL, "lng" => NULL, "status" => $status);
-                //echo "Address " . $addr . " failed to geocoded. ";
-                //echo "Received status " . $status . "<br />";
-            }
-            usleep($delay);
-        }
-        return ($geocodeInfo);
-    }
     
  /*////////////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*\
 ////////Private and Secondary Methods below this line\\\\\\\\\\\\\
--- food_collector/offers.php
+++ food_collector/offers.php
@@ -10,15 +10,14 @@ $offers =mysql_query('SELECT * FROM offer') or die ('Unable to retrieve offers')
 $num_offers = mysql_num_rows($offer);
 if ($num_offers>0){
     for ($i=0;$i<$num_offers;$i++){
-            $offer_ID=mysql_result($offers, $i, 'Offer_ID');
-            $supplier_ID=mysql_result($offers, $i, 'supplier_ID');
+            $offer_ID=mysql_result($offers, $i, 'offer_id');
+            $supplier_ID=mysql_result($offers, $i, 'supplier_id');
             $descrption=mysql_result($offers, $i, 'description');
-            $avail_date=mysql_result($offers, $i, 'av_date');
-            $avial_time=mysql_result($offers, $i, 'av_time');
-            $exp_date=mysql_result($offers, $i, 'exp_date');
-            $exp_time=mysql_result($offers, $i, 'exp_time');
+            $avail_date=mysql_result($offers, $i, 'available_date');
+            $avial_time=mysql_result($offers, $i, 'available_time');
+            $exp_date=mysql_result($offers, $i, 'expiry_date');
             $status=mysql_result($offers, $i, 'status');
-            $collector_ID=mysql_result($offers, $i, 'collector_ID');
+            $collector_ID=mysql_result($offers, $i, 'collector_id');
             echo $offer_ID.','.$supplier_ID.','.$descrption.','.$avail_date.','.$avial_time.','.$exp_date.','.$exp_time.','.$status.','.$collector_ID.'<br>';
     }
 }
