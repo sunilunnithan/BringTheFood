9907a431a74c29060393ccf379c915d9bc34e01c
Aaron <aaron.ciaghi@gmail.com>
2011-07-13 12:10:49 +0200

    minor refactoring

---
+++ app/views/Account.js
@@ -0,0 +1,122 @@
+bringthefood.views.Account = Ext.extend(Ext.form.FormPanel, {
+    title: 'Your account',
+    id: 'accountform',
+    scroll: 'vertical',
+    url: 'include/account.php',
+    standardSubmit: false,
+    dockedItems:[{
+        xtype: 'toolbar',
+        title: 'Registration',
+        dock: 'top',
+        items: [
+        {
+            text: 'Back',
+            ui: 'back',
+            handler: function(){
+                Ext.dispatch({
+                    controller: bringthefood.controllers.registrationController,
+                    action: 'goBack'
+                });
+            }
+        }
+
+        ]
+    }],
+    items: [{
+        xtype: 'fieldset',
+        title: 'Personal Information',
+        items: [{
+            xtype: 'textfield',
+            name: 'name',
+            label: '(Company) Name',
+            autoCapitalize : true,
+            required: true,
+            useClearIcon: true
+        }, {
+            xtype: 'passwordfield',
+            name: 'password',
+            label: 'Password',
+            useClearIcon: true,
+            required: true
+        },{
+            xtype: 'passwordfield',
+            name: 'password2',
+            label: 'Re-enter Password',
+            useClearIcon: true,
+            required: true
+        },
+        {
+            xtype: 'emailfield',
+            name: 'email',
+            label: 'Email',
+            useClearIcon: true,
+            required: true
+        }]
+    },{
+        xtype: 'fieldset',
+        title: 'Address',
+        defaults: {
+            xtype: 'textfield'
+        },
+        items: [{
+            name: 'street',
+            label: 'Street'
+        }, {
+            name: 'city',
+            label: 'City'
+        }, {
+            name: 'zip',
+            label: 'ZIP Code',
+            vtype: 'zip'
+        }, {
+            name: 'country',
+            label: 'Country'
+        },{
+            name: 'phone',
+            label: 'Phone',
+            vtype: 'phone'
+        }]
+    },{
+        xtype: 'selectfield',
+        name: 'role',
+        label: 'Role',
+        options: [
+        {
+            text:'Supplier',
+            value:'supplier'
+        } ,
+{
+            text:'Collector',
+            value:'collector'
+        },
+
+        {
+            text:'Distributor',
+            value:'distributor'
+        }
+        ]
+    }, {
+        layout: 'hbox',
+        defaults: {
+            xtype: 'button',
+            flex: 1,
+            style: 'margin: .5em;'
+        },
+        items: [{
+            text: 'Reset',
+            handler: function(){
+                Ext.getCmp('regform').reset();
+            }
+        },
+        {
+            text: 'Submit',
+            ui: 'confirm',
+            handler: function(){
+                Ext.dispatch({
+                    controller: bringthefood.controllers.registrationController,
+                    action: 'register'
+                });
+            }
+        }]
+    }]
+});
