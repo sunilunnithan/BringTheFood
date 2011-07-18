27fb8780991beb39dcbdbf804b63812150405e03
birhanum <birhanum@walia.local>
2011-07-18 15:48:54 +0200

    rank for suppliers added

--- include/rewards.php
+++ include/rewards.php
@@ -16,6 +16,23 @@ function get_supplier_score() {
     return $score;
 }
 
+
+function get_supplier_rank(){
+    $supplier_id = $_SESSION['demo']['user_id'];
+    $rank = 0;
+    $people_served_sorted = mysql_query("SELECT supplier_id,SUM(people_served) AS served_so_far FROM offer WHERE status='collected' GROUP BY supplier_id ORDER BY served_so_far DESC");
+    $n =mysql_num_rows($people_served_sorted);
+    for ($i=0;$i<$n;$i++){
+        if (mysql_result($people_served_sorted,$i, 'supplier_id')==$supplier_id){
+            $rank=$i;
+            break;
+        }
+    }
+    return $rank+1;
+}
+
+
 $score = get_supplier_score();
-echo json_encode(array("success" => true, "score" => $score));
+$rank = get_supplier_rank();
+echo json_encode(array("success" => true, "score" => $score,"rank"=>$rank));
 ?>
