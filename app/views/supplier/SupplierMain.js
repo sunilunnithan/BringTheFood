e60a5dbbb9ef2687138caf82d72b17d7130eb00b
Aaron <aaron.ciaghi@gmail.com>
2011-07-21 15:55:44 +0200

    I don't know what's going ongit pull

--- app/views/supplier/SupplierMain.js
+++ app/views/supplier/SupplierMain.js
@@ -25,7 +25,7 @@ bringthefood.views.SupplierMain = Ext.extend(Ext.Panel, {
     }],
     items: [
     {
-        xtype: 'panel',
+        xtype: 'component',
         id: 'welcome',
         tpl: [
         '<div class="welcome">Welcome, ',
